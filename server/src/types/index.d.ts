cb5f8312e8eff0ec7db85c38b4f7e47a6c9156ae
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-09-02 13:08:33 +0545

    extend the Express Request interface

---
+++ server/src/types/index.d.ts
@@ -0,0 +1,11 @@
+import 'express';
+import { IUser } from '../schemas/user.schema';
+
+declare global {
+  namespace Express {
+    interface Request {
+      token?: string;
+      user?: IUser;
+    }
+  }
+}
