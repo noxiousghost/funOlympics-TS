f67e92cad0b3c5cbf2e9f78b6f6663d08be83875
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-09-03 13:05:55 +0545

    use tokenExtrator and userExatrator middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,17 @@
 import app from './configs/express.config';
 import { envVars } from './configs/envVars.config';
 import { logger, morganMiddleware } from './configs/logger.config';
+import {
+  userExtractor,
+  tokenExtractor,
+} from './middlewares/authentication.middleware';
 import DbConnection from './configs/db.config';
 import userRouter from './routes/user.route';
 const port = envVars.PORT;
 DbConnection();
 app.use(morganMiddleware());
+app.use(tokenExtractor);
+app.use(userExtractor);
 app.use('/api/users', userRouter);
 app.listen(port, () => {
   logger.info(`App listening on port ${port}`);
