362df7d80e32216012eeb59d71684e69591b5151
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-09-03 13:07:08 +0545

    add route for login

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,15 +1,19 @@
 // src/routes/user.routes.ts
 import { Router } from 'express';
-import { checkAdmin } from '../middlewares/authentication.middleware';
+import {
+  checkAdmin,
+  userExtractor,
+} from '../middlewares/authentication.middleware';
 import * as UserController from '../controllers/user.controller';
 
 const userRouter = Router();
 
-userRouter.get('/', checkAdmin, UserController.getAllUsers);
+userRouter.get('/', userExtractor, checkAdmin, UserController.getAllUsers);
 userRouter.get('/:id', UserController.getUserById);
 userRouter.post('/', UserController.createUser);
 userRouter.post('/verify', UserController.verifyUser);
 userRouter.patch('/:id', UserController.updateUser);
 userRouter.patch('/fav/:id', UserController.updateFavourites);
+userRouter.post('/login', UserController.loginUser);
 
 export default userRouter;
