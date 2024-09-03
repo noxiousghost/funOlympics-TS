41dbcccaf6e8f1a2e5e90bb27b7e243562f57236
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-09-03 09:24:35 +0545

    create middleware for handling errors

---
+++ server/src/middlewares/errorHandlers.middleware.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+import { logger } from '../configs/logger.config';
+
+export const errorHandler = (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  logger.error(error.message);
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message });
+  } else if (error.name === 'JsonWebTokenError') {
+    return res.status(401).json({ error: 'invalid token' });
+  } else if (error.name === 'TokenExpiredError') {
+    return res.status(401).json({ error: 'token expired' });
+  }
+
+  next(error);
+};
