6e9e6edfd5c7f0f5b50ef263bb7b5b339c1898ea
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-09-03 09:26:50 +0545

    integrate user route

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,11 @@ import app from './configs/express.config';
 import { envVars } from './configs/envVars.config';
 import { logger, morganMiddleware } from './configs/logger.config';
 import DbConnection from './configs/db.config';
+import userRouter from './routes/user.route';
 const port = envVars.PORT;
 DbConnection();
 app.use(morganMiddleware());
+app.use('/api/users', userRouter);
 app.listen(port, () => {
   logger.info(`App listening on port ${port}`);
 });
