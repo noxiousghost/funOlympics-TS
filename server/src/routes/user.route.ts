57102a4cb7db647a03cbb3bb2329c53905add6d1
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-09-04 12:45:49 +0545

    use rateLimit middleware in the login and signup route

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,18 +1,17 @@
 import { Router } from 'express';
-import {
-  checkAdmin,
-  userExtractor,
-} from '../middlewares/authentication.middleware';
+import { checkAdmin } from '../middlewares/authentication.middleware';
 import { validateRequest } from '../middlewares/validation.middleware';
+import { rateLimiter } from '../middlewares/rateLimit.middleware';
 import { registerSchema, loginSchema } from '../constants/user.validation';
 import * as UserController from '../controllers/user.controller';
 
 const userRouter = Router();
 
-userRouter.get('/', userExtractor, checkAdmin, UserController.getAllUsers);
+userRouter.get('/', checkAdmin, UserController.getAllUsers);
 userRouter.get('/:id', UserController.getUserById);
 userRouter.post(
   '/',
+  rateLimiter,
   validateRequest(registerSchema),
   UserController.createUser,
 );
@@ -21,6 +20,7 @@ userRouter.patch('/:id', UserController.updateUser);
 userRouter.patch('/fav/:id', UserController.updateFavourites);
 userRouter.post(
   '/login',
+  rateLimiter,
   validateRequest(loginSchema),
   UserController.loginUser,
 );
