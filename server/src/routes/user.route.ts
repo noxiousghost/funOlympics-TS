14ea244c05913e567b9241a60bd377ea10ad08d7
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-09-04 12:16:27 +0545

    add validation middeware and function to signup and login route

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,19 +1,28 @@
-// src/routes/user.routes.ts
 import { Router } from 'express';
 import {
   checkAdmin,
   userExtractor,
 } from '../middlewares/authentication.middleware';
+import { validateRequest } from '../middlewares/validation.middleware';
+import { registerSchema, loginSchema } from '../constants/user.validation';
 import * as UserController from '../controllers/user.controller';
 
 const userRouter = Router();
 
 userRouter.get('/', userExtractor, checkAdmin, UserController.getAllUsers);
 userRouter.get('/:id', UserController.getUserById);
-userRouter.post('/', UserController.createUser);
+userRouter.post(
+  '/',
+  validateRequest(registerSchema),
+  UserController.createUser,
+);
 userRouter.post('/verify', UserController.verifyUser);
 userRouter.patch('/:id', UserController.updateUser);
 userRouter.patch('/fav/:id', UserController.updateFavourites);
-userRouter.post('/login', UserController.loginUser);
+userRouter.post(
+  '/login',
+  validateRequest(loginSchema),
+  UserController.loginUser,
+);
 
 export default userRouter;
