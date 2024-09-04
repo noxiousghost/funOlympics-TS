18bfb80d1817cf0cadc869970ebdef637a7ce3d3
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-09-04 12:40:10 +0545

    add better error handling for joi

--- server/src/middlewares/validation.middleware.ts
+++ server/src/middlewares/validation.middleware.ts
@@ -1,11 +1,32 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response, NextFunction } from 'express';
 import Joi from 'joi';
 
 export const validateRequest = (schema: Joi.ObjectSchema) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    const { error } = schema.validate(req.body);
+    const { error } = schema.validate(req.body, { abortEarly: false });
     if (error) {
-      return res.status(400).json({ error: error.details[0].message });
+      const errors = error.details.reduce((acc: any, detail) => {
+        const key = detail.context?.key || 'unknownField';
+        if (!acc[key]) {
+          acc[key] = [];
+        }
+        switch (detail.type) {
+          case 'string.empty':
+          case 'any.required':
+            acc[key].push(`${key} is required.`);
+            break;
+          case 'string.pattern.base':
+            acc[key].push(`${key} is not formatted correctly.`);
+            break;
+          default:
+            acc[key].push(`Invalid value provided for ${key}.`);
+            break;
+        }
+        return acc;
+      }, {});
+
+      return res.status(400).json({ errors });
     }
     next();
   };
