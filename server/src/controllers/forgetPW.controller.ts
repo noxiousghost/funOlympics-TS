92474a3e6fa5f53f03da192a652c4a996ae9f67c
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-09-14 15:48:13 +0545

    implement better logic and error handling

--- server/src/controllers/forgetPW.controller.ts
+++ server/src/controllers/forgetPW.controller.ts
@@ -1,33 +1,23 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from 'express';
 import * as PasswordService from '../services/forgetPW.service';
+import { catchAsync } from '../middlewares/errorHandlers.middleware';
 
-export const sendResetEmail = async (req: Request, res: Response) => {
-  try {
+export const sendResetEmail = catchAsync(
+  async (req: Request, res: Response) => {
     await PasswordService.requestPasswordReset(req.body.email);
     res.status(201).json({ message: 'Change password OTP sent.' });
-  } catch (error: any) {
-    res.status(400).json({ error: error.message });
-  }
-};
+  },
+);
 
-export const verifyOtpCode = async (req: Request, res: Response) => {
-  try {
-    const user = await PasswordService.verifyOtp(req.body.email, req.body.code);
-    res.status(200).json(user);
-  } catch (error: any) {
-    res.status(400).json({ error: error.message });
-  }
-};
+export const verifyOtpCode = catchAsync(async (req: Request, res: Response) => {
+  await PasswordService.verifyOtp(req.body.email, req.body.code);
+  res.status(200).json({ message: 'OTP verified successfully!' });
+});
 
-export const changePassword = async (req: Request, res: Response) => {
-  try {
-    const user = await PasswordService.updatePassword(
-      req.body.email,
-      req.body.password,
-    );
-    res.status(200).json(user);
-  } catch (error: any) {
-    res.status(400).json({ error: error.message });
-  }
-};
+export const changePassword = catchAsync(
+  async (req: Request, res: Response) => {
+    await PasswordService.updatePassword(req.body.email, req.body.password);
+    res.status(200).json({ message: 'Password changed successfully' });
+  },
+);
--- server/src/services/forgetPW.service.ts
+++ server/src/services/forgetPW.service.ts
@@ -2,22 +2,27 @@ import User from '../schemas/user.schema';
 import FpMail from '../schemas/fpMail.schema';
 import Bcrypt from 'bcrypt';
 import FpMailSender from '../utils/fpMailSender.util';
+import { AppError } from '../middlewares/errorHandlers.middleware';
 
 export const requestPasswordReset = async (email: string) => {
   const user = await User.findOne({ email });
   if (!user) {
-    throw new Error("User doesn't exist!");
+    throw new AppError("User doesn't exist!", 401);
   }
   const sendMail = await FpMailSender.sendEmail(email);
   if (!sendMail) {
-    throw new Error('Failed to send OTP!');
+    throw new AppError('Failed to send OTP!', 400);
   }
 };
 
 export const verifyOtp = async (email: string, code: number) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new AppError('User not registered', 404);
+  }
   const fpMail = await FpMail.findOne({ email });
   if (!fpMail || fpMail.code !== code) {
-    throw new Error('Invalid OTP');
+    throw new AppError('Invalid OTP', 401);
   }
   fpMail.verified = true;
   await fpMail.save();
@@ -27,11 +32,11 @@ export const verifyOtp = async (email: string, code: number) => {
 export const updatePassword = async (email: string, newPassword: string) => {
   const user = await User.findOne({ email });
   if (!user) {
-    throw new Error('User does not exist!');
+    throw new AppError('User does not exist!', 401);
   }
   const fpMail = await FpMail.findOne({ email });
   if (!fpMail || !fpMail.verified) {
-    throw new Error('OTP code not verified!');
+    throw new AppError('OTP code not verified!', 400);
   }
   const newPasswordHash = await Bcrypt.hash(newPassword, 10);
   user.passwordHash = newPasswordHash;
