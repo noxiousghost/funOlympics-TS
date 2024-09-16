722203ceabf8829f5be0a1df3c43ff349ce76335
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-09-16 13:23:34 +0545

    create middleware for checking exiting news

--- server/src/routes/news.route.ts
+++ server/src/routes/news.route.ts
@@ -5,6 +5,7 @@ import { uploadImage } from '../configs/multer.config';
 import { setFileType } from '../middlewares/setFileType.middleware';
 import { multerErrorHandler } from '../middlewares/errorHandlers.middleware';
 import { sanitizeFileName } from '../middlewares/sanitizeFileName.middleware';
+import { newsExists } from '../middlewares/exists.middleware';
 
 const newsRouter = Router();
 
@@ -15,8 +16,9 @@ newsRouter.post(
   checkAdmin,
   setFileType,
   uploadImage,
-  multerErrorHandler,
   sanitizeFileName,
+  multerErrorHandler,
+  newsExists,
   NewsController.createNews,
 );
 newsRouter.patch('/:id', checkAdmin, NewsController.updateNews);
