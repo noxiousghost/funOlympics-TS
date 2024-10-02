3ba6b00262a387f2e9962d8b6d7d650603555ab8
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-10-02 22:39:10 +0545

    used AppError to throw error

--- server/src/middlewares/unknownEndpoint.middleware.ts
+++ server/src/middlewares/unknownEndpoint.middleware.ts
@@ -1,5 +1,10 @@
-import { Request, Response } from 'express';
-
-export const unknownEndpoint = (req: Request, res: Response) => {
-  res.status(404).send({ error: 'unknown endpoint' });
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Request, Response, NextFunction } from 'express';
+import { AppError } from './errorHandlers.middleware';
+export const unknownEndpoint = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  throw new AppError(`Can't find ${req.originalUrl} on this server`, 404);
 };
